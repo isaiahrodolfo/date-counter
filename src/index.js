e00aab2ae1e8256b51feec5d646f989c396d62ec
Isaiah <irodol@uw.edu>
2025-01-08 16:20:28 -0800

    Completed solution

--- src/index.js
+++ src/index.js
@@ -1,70 +1,64 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+// import "./styles.css";
 
 function App() {
-  const skills = [
-    ['React', 'R'],
-    ['React Native', 'RN'],
-    ['Javascript', 'JS'],
-    ['Typescript', 'TS']
-  ]
+  const [step, setStep] = useState(1);
+  const [dayInterval, setDayInterval] = useState(0); // today
+
+  const date = new Date(); // now
+  date.setDate(date.getDate() + dayInterval);
 
   return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
-        <SkillsList skills={skills} />
-      </div>
+    <div>
+      <Counter
+        text={"Step"}
+        num={step}
+        increaseBy={1}
+        onIncrease={() => setStep(step + 1)}
+        onDecrease={() => setStep(step - 1)}
+      />
+      <Counter
+        text={"Count"}
+        num={dayInterval}
+        increaseBy={step}
+        onIncrease={() => setDayInterval(dayInterval + step)}
+        onDecrease={() => setDayInterval(dayInterval - step)}
+      />
+      The date {dayInterval} days from today is {date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" })}
     </div>
   );
 }
 
-function Avatar() {
+function Counter({ text, num, increaseBy, onIncrease, onDecrease }) {
+  const handleIncrease = () => {
+    if (onIncrease) {
+      onIncrease();
+    }
+  };
+
+  const handleDecrease = () => {
+    if (onDecrease) {
+      onDecrease();
+    }
+  };
+
   return (
-    <div className="avatar">
-      <img src="../avatar.png" alt="Me" />
+    <div>
+      <h3>{text}: {num}</h3>
+      <button onClick={handleIncrease}>Increase by {increaseBy}</button>
+      <button onClick={handleDecrease}>Decrease by {increaseBy}</button>
     </div>
-  )
+  );
 }
 
-function Intro() {
-  return (
-    <>
-      <h1>Isaiah Rodolfo</h1>
-      <body>
-        Hi, I am Isaiah and I love coding in React!
-      </body>
-    </>
-  )
-}
 
-function SkillsList({ skills }) {
 
-  function Skill({ skill }) {
-    return (
-      <div skill style={{ backgroundColor: 'lightgray', borderRadius: 15, padding: 8 }}>
-        {skill[0]} {skill[1]}
-      </div>
-    )
-  }
-
-  return (
-    <div className="skill-list">
-      {skills.map((skill) => (<Skill skill={skill} />))}
-    </div>
-  )
-}
 
 
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
 root.render(
   <StrictMode>
     <App />
